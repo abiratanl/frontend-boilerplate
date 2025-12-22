df450fbe59933811c068a397cbefdd2625f5c130
Abiratan Lopes <abi.lopes.sousa@gmail.com>
2025-12-22 01:28:59 -0300

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // <--- A MUDANÇA É AQUI (tem o @ e a /)
+    autoprefixer: {},
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    // Esta div ocupa a tela toda (min-h-screen) e centraliza o conteúdo
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      
+      {/* Este h1 tem texto grande (3xl), negrito (bold) e cor azul */}
+      <h1 className="text-3xl font-bold text-blue-600">
+        Boilerplate Pronto 🚀
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
